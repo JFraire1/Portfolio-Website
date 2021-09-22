712b1c235169ea2ac42993ae5de7f9fdcc29ee26
JFraire1 <Jose.Fraire@student.csulb.edu>
2021-09-22 16:30:23 -0700

    adding functionality for contact form

---
+++ php/contact.php
@@ -0,0 +1,24 @@
+<?php
+
+if (isset($_POST['submit'])){
+
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $mailFrom = $_POST['mail'];
+    $message = $_POST['message'];
+
+    $mailTo = "Jose.Fraire@student.csulb.edu";
+    $headers = "From: Website Email From ".$mailFrom;
+    $txt = "You got an email from ".$name.". \n\n".$message;
+
+    if(mail($mailTo, $subject, $txt, $headers)){
+        echo "The email($email_subject) was successfully sent.";
+    }
+    else {
+        echo "The email($email_subject) was NOT sent.";
+    }
+
+    //header('Location: /~026422862/');
+    //exit;
+
+}
