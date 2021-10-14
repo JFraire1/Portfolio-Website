47ac59c95b9565ddbc820fbc657ce33e0bec8c9b
JFraire1 <Jose.Fraire@student.csulb.edu>
2021-10-13 23:18:51 -0700

    temp fixes

--- php/contact.php
+++ php/contact.php
@@ -12,14 +12,13 @@ if (isset($_POST['submit'])){
     $txt = "You got an email from ".$name.". \n\n".$message;
 
     if(mail($mailTo, $subject, $txt, $headers)){
-        echo "The email($email_subject) was successfully sent.";
+        header('Location: /~026422862/');
+        exit;
     }
     else {
         echo "The email($email_subject) was NOT sent.";
-        echo "Currently Working With System Admin to Resolve The Issue, Please Email Me Directly, For Now"
+        echo "Currently Working With System Admin to Resolve The Issue, Please Email Me Directly, For Now";
+        exit;
     }
-
-    //header('Location: /~026422862/');
-    //exit;
 
 }
